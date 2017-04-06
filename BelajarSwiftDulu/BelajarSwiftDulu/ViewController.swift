8c8e7c70098351b3e9eac852ed3837e39d2db903
Laluhelmi <lhilmifadli@yahoo.co.id>
2017-04-07 01:16:05 +0700

    ini swift

---
+++ BelajarSwiftDulu/BelajarSwiftDulu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BelajarSwiftDulu
+//
+//  Created by hazard on 4/7/17.
+//  Copyright © 2017 hazard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
